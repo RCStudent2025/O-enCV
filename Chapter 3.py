edb5fa12320aeba1f35bb8db94fc91197fcdc1f7
Robotics Student <roboticsstudent1@gmail.com>
2025-11-30 17:00:33 +0800

    Add files via upload

---
+++ Chapter 3.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("lambo.png")
+print(img.shape)
+imgResize = cv2.resize(img, (300, 200))
+print(imgResize.shape)
+imgCropped = img[0:200, 200:500]
+
+cv2.imshow("Image", img)
+cv2.imshow("Resized Image", imgResize)
+cv2.imshow("Cropped Image", imgCropped)
+
+cv2.waitKey(0)
